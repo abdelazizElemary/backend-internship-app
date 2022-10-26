bae47c4d967c152fdbd617ff0d4b640ad57766a2
Ahmed Elsayed <ahm.abdelwahab@nu.edu.eg>
2022-10-26 17:16:51 +0200

    add and get specific/all books

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ApiOverview, name='home')
+    path('', views.ApiOverview, name='home'),
+    path('add/book/', views.add_book, name='add-book'),
+    path('all/', views.view_books, name='view-books'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
 
+from unicodedata import category
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import serializers
+from rest_framework import status
 
 
 @api_view(['GET'])
@@ -10,9 +13,44 @@ def ApiOverview(request):
     api_urls = {
         'all_items': '/',
         'Search by Category': '/?category=category_name',
-        'Add': '/create',
+        'Add': '/add/book',
         'Update': '/update/pk',
         'Delete': '/item/pk/delete'
     }
 
     return Response(api_urls)
+
+
+@api_view(['POST'])
+def add_book(request):
+    item = BookSerializer(data=request.data)
+
+    # validating for already existing data
+    if Book.objects.filter(**request.data).exists():
+        raise serializers.ValidationError('This data already exists')
+
+    if item.is_valid():
+        item.save()
+        return Response(item.data)
+    else:
+        return Response(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+
+@api_view(['GET'])
+def view_books(request):
+
+    # checking for the parameters from the URL
+    # get a specific book (by category) or get all books
+    if request.query_params:
+        category = request.query_params.get("category", None)
+        if category:
+            items = Book.objects.filter(category=category)
+    else:
+        items = Book.objects.all()
+
+    # if there is something in items else raise error
+    if items:
+        serializer = BookSerializer(items, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
