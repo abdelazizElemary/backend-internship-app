266123d5dfbe75feda1dde638e73a17d8b6f4a56
M7mmed-Sayed <mohamedsayed1167@gmail.com>
2022-11-07 15:32:06 +0200

    add LogoutView API to my App

--- library/users/urls.py
+++ library/users/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path, include
 from rest_framework.authtoken import views as auth_view
+from knox import views as knox_views
 
 from .views import login_view, user_profile_view, Signup_view, user_change_password_view
 
 urlpatterns = [
     path('login/', login_view),
     path('profile/', user_profile_view),
+    path('logout/', knox_views.LogoutView.as_view()),
     path('signup/', Signup_view),
     path('change-password/', user_change_password_view),
 
 ]
 """".venv
-pycache
+pycache 
 .env
 .sql
 Abdelaziz Elemary8:39 PM
